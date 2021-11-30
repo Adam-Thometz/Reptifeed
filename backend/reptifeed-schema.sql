6f161146d796f664a6a772fa06fa5cacaf92a8d4
Adam Thometz <adamthometz@gmail.com>
2021-11-30 09:21:42 -0500

    initialized front end, added schema, started scraping functions

---
+++ backend/reptifeed-schema.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE "users" (
+    "id" int   NOT NULL,
+    "username" string   NOT NULL,
+    "password" string   NOT NULL,
+    "email" string   NOT NULL,
+    CONSTRAINT "pk_users" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "reptiles" (
+    "id" int   NOT NULL,
+    "name" string   NOT NULL,
+    "birthday" date   NOT NULL,
+    "img_url" string   NOT NULL,
+    "owner" string   NOT NULL,
+    CONSTRAINT "pk_reptiles" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "reptiles" ADD CONSTRAINT "fk_reptiles_owner" FOREIGN KEY("owner")
+REFERENCES "users" ("id");
+
