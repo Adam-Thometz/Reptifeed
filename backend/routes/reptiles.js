eac5c523091b6af4b63aae9b46c18dd8ff09de7f
Adam Thometz <adamthometz@gmail.com>
2021-12-15 17:41:26 -0500

    added reptile routes and tests

---
+++ backend/routes/reptiles.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const router = express.Router();
+const Reptile = require('../models/reptile');
+const jsonschema = require('jsonschema');
+const reptileNewSchema = require('../schemas/reptileNewSchema.json')
+const reptileUpdateSchema = require('../schemas/reptileUpdateSchema.json')
+const { BadRequestError, UnauthorizedError } = require('../expressError');
+const { ensureAdminOrCorrectOwner, ensureAdmin } = require('../middleware/auth');
+
+const bodyParser = require('body-parser')
+
+router.use(bodyParser.json());
+
+/** POST /reptiles
+ * 
+ * Creates a new reptile
+ * 
+ * Admin/correct user auth required
+ */
+router.post('/', ensureAdminOrCorrectOwner, async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, reptileNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    };
+
+    const reptile = await Reptile.create(req.body);
+    return res.status(201).json({ reptile });
+  } catch (err) {
+    return next(err);
+  };
+});
+
+/** GET /reptiles
+ * 
+ * Returns a list of all reptiles
+ * 
+ * Admin auth required
+ */
+router.get('/', ensureAdmin, async (req, res, next) => {
+  try {
+    const reptiles = await Reptile.getAll();
+    return res.json({ reptiles })
+  } catch (err) {
+    return next(err);
+  };
+});
+
+/** GET /reptiles/:id
+ * 
+ * Returns a single reptile
+ * 
+ * Admin/correct user auth required
+ */
+router.get('/:id', ensureAdminOrCorrectOwner, async (req, res, next) => {
+  try {
+    const reptile = await Reptile.get(+req.params.id);
+    return res.json({ reptile })
+  } catch (err) {
+    return next(err);
+  };
+});
+
+/** PATCH /reptiles/:id
+ * 
+ * Edits a single reptile. Valid fields are name, subspecies, birthday, and imgUrl
+ * 
+ * Admin/correct user auth required
+ */
+router.patch('/:id', ensureAdminOrCorrectOwner, async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, reptileUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const reptile = await Reptile.update(req.params.id, req.body);
+    return res.json({ reptile })
+  } catch (err) {
+    return next(err);
+  };
+});
+
+/** DELETE /reptiles/:id
+ * 
+ * Deletes a single reptile
+ * 
+ * Admin/correct user auth required
+ */
+router.delete('/:id', ensureAdminOrCorrectOwner, async (req, res, next) => {
+  try {
+    const reptile = await Reptile.delete(req.params.id);
+    return res.json({ deleted: reptile });
+  } catch (err) {
+    return next(err);
+  };
+});
+
+module.exports = router;
---
+++ backend/routes/reptiles.test.js
@@ -0,0 +1,342 @@
+"use strict";
+
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config')
+
+const app = require('../app');
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  testUserIds, // use idx 0 for user, idx 1 for admin
+  tokens, // use idx 0 for user, idx 1 for admin
+  testReptileIds
+} = require('./_testCommon');
+const { UnauthorizedError } = require('../expressError');
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/** POST /reptiles */
+describe('create new reptile', () => {
+  test('works for correct user', async () => {
+    const newReptile = {
+      name: "mary",
+      species: "snail",
+      subspecies: "mean sea snail",
+      birthday: '2010-05-01',
+      imgUrl: 'image of mary',
+      ownerId: testUserIds[0]
+    }
+    const token = tokens[0];
+    const res = await request(app)
+      .post('/reptiles')
+      .send(newReptile)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual( { reptile: { ...newReptile, id: expect.any(Number) } });
+  });
+
+  test('works for admin', async () => {
+    const newReptile = {
+      name: "mary",
+      species: "snail",
+      subspecies: "mean sea snail",
+      birthday: '2010-05-01',
+      imgUrl: 'image of mary',
+      ownerId: testUserIds[0]
+    };
+    const token = tokens[1];
+    const res = await request(app)
+      .post('/reptiles')
+      .send(newReptile)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual( { reptile: { ...newReptile, id: expect.any(Number) } });
+  });
+
+  test('throws unauth for incorrect user', async () => {
+    const newReptile = {
+      name: "mary",
+      species: "snail",
+      subspecies: "mean sea snail",
+      birthday: '2010-05-01',
+      imgUrl: 'image of mary',
+      ownerId: testUserIds[1]
+    };
+    const token = tokens[0];
+    const res = await request(app)
+      .post('/reptiles')
+      .send(newReptile)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(401);
+  })
+
+  test('throws unauth for anon', async () => {
+    const newReptile = {
+      name: "mary",
+      species: "snail",
+      subspecies: "mean sea snail",
+      birthday: '2010-05-01',
+      imgUrl: 'image of mary',
+      ownerId: testUserIds[0]
+    };
+    const res = await request(app)
+      .post('/reptiles')
+      .send(newReptile);
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test('throws bad request for missing information', async () => {
+    const newReptile = {
+      name: "mary",
+      imgUrl: 'image of mary',
+      ownerId: testUserIds[0]
+    };
+    const token = tokens[0]
+    const res = await request(app)
+      .post('/reptiles')
+      .send(newReptile)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(400);
+  })
+});
+
+/** GET /reptiles */
+describe('get all reptiles', () => {
+  test('works for admin', async () => {
+    const token = tokens[1];
+    const res = await request(app)
+      .get('/reptiles')
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual({
+      reptiles: [
+        {
+          id: expect.any(Number),
+          name: 'gary',
+          species: 'snail',
+          subspecies: 'sea snail',
+          birthday: '2010-01-01',
+          imgUrl: 'picture of gary',
+          ownerId: testUserIds[0]
+        },
+        {
+          id: expect.any(Number),
+          name: 'larry',
+          species: 'snail',
+          subspecies: 'mean sea snail',
+          birthday: '2015-01-01',
+          imgUrl: 'picture of larry',
+          ownerId: testUserIds[1]
+        }
+      ]
+    });
+  });
+
+  test('throws unauth for non-admin users', async () => {
+    const token = tokens[0];
+    const res = await request(app)
+      .get('/reptiles')
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(401);
+  });
+  
+  test('throws unauth for anon', async () => {
+    const res = await request(app)
+      .get('/reptiles')
+    expect(res.statusCode).toEqual(401);
+  });
+});
+
+describe('get reptile by id', () => {
+  test('success for user', async  () => {
+    const id = testReptileIds[0];
+    const token = tokens[0];
+    const res = await request(app)
+      .get(`/reptiles/${id}`)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual({
+      reptile: {
+        id,
+        name: 'gary',
+        species: 'snail',
+        subspecies: 'sea snail',
+        birthday: '2010-01-01',
+        imgUrl: 'picture of gary',
+        ownerId: testUserIds[0]
+      }
+    });
+  });
+
+  test('success for admin', async  () => {
+    const id = testReptileIds[0];
+    const token = tokens[1];
+    const res = await request(app)
+      .get(`/reptiles/${id}`)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual({
+      reptile: {
+        id: expect.any(Number),
+        name: 'gary',
+        species: 'snail',
+        subspecies: 'sea snail',
+        birthday: '2010-01-01',
+        imgUrl: 'picture of gary',
+        ownerId: testUserIds[0]
+      }
+    });
+  });
+
+  test('throws unauth if neither admin nor correct user', async () => {
+    const id = testReptileIds[1];
+    const token = tokens[0];
+    const res = await request(app)
+      .get(`/reptiles/${id}`)
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test('throws unauth if anon', async () => {
+    const id = testReptileIds[1];
+    const res = await request(app)
+      .get(`/reptiles/${id}`)
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test('throws not found if no such reptile', async () => {
+    const token = tokens[1];
+    const res = await request(app)
+      .get('/reptiles/-1')
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(404);
+  });
+});
+
+
+describe('update a reptile by id', () => {
+  test('success for owner', async  () => {
+    const id = testReptileIds[0];
+    const token = tokens[0];
+    const res = await request(app)
+      .patch(`/reptiles/${id}`)
+      .send({ birthday: '1997-05-01' })
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual({
+      reptile: {
+        id,
+        name: 'gary',
+        species: 'snail',
+        subspecies: 'sea snail',
+        birthday: '1997-05-01',
+        imgUrl: 'picture of gary',
+        ownerId: testUserIds[0]
+      }
+    });
+  });
+
+  test('success for admin', async  () => {
+    const id = testReptileIds[0];
+    const token = tokens[1];
+    const res = await request(app)
+      .patch(`/reptiles/${id}`)
+      .send({ birthday: '1997-05-01' })
+      .set('authorization', `Bearer ${token}`);
+    expect(res.body).toEqual({
+      reptile: {
+        id,
+        name: 'gary',
+        species: 'snail',
+        subspecies: 'sea snail',
+        birthday: '1997-05-01',
+        imgUrl: 'picture of gary',
+        ownerId: testUserIds[0]
+      }
+    });
+  });
+
+  test('throws unauth if not owner', async () => {
+    const id = testReptileIds[0];    
+    const token = tokens[0];
+    const res = await request(app)
+      .patch(`/reptiles/${id+1}`)
+      .send({ name: 'gery' })
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test('throws unauth if anon', async () => {
+    const id = testReptileIds[0];   
+    const res = await request(app)
+      .patch(`/reptiles/${id}`)
+      .send({ name: 'gery' });
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test('throws bad request if invalid data', async () => {
+    const id = testReptileIds[0];
+    const token = tokens[0];
+    const res = await request(app)
+      .patch(`/reptiles/${id}`)
+      .send({ date: 'today-is-monday' })
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(400);
+  });
+  
+  test('throws bad request if invalid fields', async () => {
+    const id = testReptileIds[0];
+    const token = tokens[0];
+    const res = await request(app)
+      .patch(`/reptiles/${id}`)
+      .send({ neverGonna: 'let-you-down' })
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(400);
+  });
+
+  test('throws not found if no such reptile', async () => {
+    const token = tokens[0];
+    const res = await request(app)
+      .patch('/reptiles/-1')
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(404)
+  });
+});
+
+describe('delete a reptile by id', () => {
+  test('success for owner', async () => {
+    const id = testReptileIds[0];
+    const token = tokens[0];
+    const res = await request(app)
+      .delete(`/reptiles/${id}`)
+      .set('authorization', `Bearer ${token}`)
+    expect(res.body).toEqual({ deleted: 'gary' });
+  });
+  
+  test('success for admin', async () => {
+    const id = testReptileIds[0];
+    const token = tokens[1];
+    const res = await request(app)
+      .delete(`/reptiles/${id}`)
+      .set('authorization', `Bearer ${token}`)
+    expect(res.body).toEqual({ deleted: 'gary' });
+  });
+
+  test('throws unauth if neither owner nor admin', async () => {
+    const id = testReptileIds[1];
+    const token = tokens[0];
+    const res = await request(app)
+      .delete(`/reptiles/${id}`)
+      .set('authorization', `Bearer ${token}`)
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test('throws not found if no such reptile', async () => {
+    const token = tokens[1];
+    const res = await request(app)
+      .delete('/reptiles/-1')
+      .set('authorization', `Bearer ${token}`);
+    expect(res.statusCode).toEqual(404);
+  });
+});
