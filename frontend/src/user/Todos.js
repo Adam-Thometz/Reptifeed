cb993b26b749daff9d6c48d14b70ec792d4a457b
Adam Thometz <adamthometz@gmail.com>
2022-01-01 22:01:18 -0500

    fixed useLocation bug in test

--- frontend/src/user/Todos.js
+++ frontend/src/user/Todos.js
@@ -4,6 +4,7 @@ import UserContext from "../utils/UserContext";
 import './Todos.css';
 
 const Todos = () => {
+  debugger;
   const { currUser } = useContext(UserContext);
   const location = useLocation();
   const navigate = useNavigate();
@@ -18,16 +19,16 @@ const Todos = () => {
         {essentialTodos.length ? (
           <div className="Todos-wrapper essential-wrapper">
             <h4>Essential</h4>
-            {essentialTodos.map(t => (
-              <Link className="Todos-todo essential" to={(t === 'Add a reptile') ? `/users/${currUser.id}/reptiles/add` : '/foods'} >{t}</Link>
+            {essentialTodos.map((t, i) => (
+              <Link key={i} className="Todos-todo essential" to={(t === 'Add a reptile') ? `/users/${currUser.id}/reptiles/add` : '/foods'} >{t}</Link>
               ))}
           </div>
         ) : null}
         {niceToHaveTodos.length ? (
           <div className="Todos-wrapper niceToHave-wrapper">
             <h4>Nice to Have</h4>
-            {niceToHaveTodos.map(t => (
-              <Link className="Todos-todo niceToHave" to="/foods" >{t}</Link>
+            {niceToHaveTodos.map((t, i) => (
+              <Link key={i} className="Todos-todo niceToHave" to="/foods" >{t}</Link>
             ))}
           </div>
         ) : null}
--- frontend/src/user/Todos.test.js
+++ frontend/src/user/Todos.test.js
@@ -2,41 +2,30 @@ import React from "react";
 import Todos from "./Todos";
 import { render, UserProvider } from '../utils/testUtils';
 import { Routes, Route } from 'react-router-dom';
+import RequireAuth from "../routes-nav/RequireAuth";
 
 test('it renders without crashing', () => {
+  const todos = {
+    essentialTodos: ['Buy a plumbus'],
+    niceToHaveTodos: ['Figure out the plumbus']
+  };
   render(<UserProvider>
     <Routes>
-      <Route
-        location={{
-          pathname: '/users/:id/todos',
-          state: {
-            todos: {
-              essentialTodos: ['Buy a plumbus'],
-              niceToHaveTodos: ['Figure out the plumbus']
-            }
-          }
-        }} element={<Todos />}
-      />
+      <Route path='/users/:id/todos' element={<RequireAuth><Todos /></RequireAuth>} />
     </Routes>
-  </UserProvider>, { initialRoutes: ['/users/1/todos'] });
+  </UserProvider>, { initialRoutes: [{ pathname: '/users/1/todos', state: { todos } }] });
 });
 
 test('it matches snapshot', () => {
+  const todos = {
+    essentialTodos: ['Buy a plumbus'],
+    niceToHaveTodos: ['Figure out the plumbus']
+  };
   const { asFragment } = render(
     <UserProvider>
       <Routes>
-        <Route
-          location={{
-            pathname: '/users/:id/todos',
-            state: {
-              todos: {
-                essentialTodos: ['Buy a plumbus'],
-                niceToHaveTodos: ['Figure out the plumbus']
-              }
-            }
-          }} element={<Todos />}
-        />
+        <Route path='/users/:id/todos' element={<RequireAuth><Todos /></RequireAuth>} />
       </Routes>
-    </UserProvider>, { initialRoutes: ['/users/1/todos'] });
+    </UserProvider>, { initialRoutes: [{ pathname: '/users/1/todos', state: { todos } }] });
   expect(asFragment()).toMatchSnapshot();
 });
