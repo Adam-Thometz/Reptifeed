c8217a568d9f4227bba9da5e870bebf5c4b7478f
Adam Thometz <adamthometz@gmail.com>
2022-01-04 09:24:42 -0500

    added error handling for incomplete pantry

--- frontend/src/reptile/Reptile.js
+++ frontend/src/reptile/Reptile.js
@@ -1,26 +1,51 @@
-import React, { useState, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import React, { useState, useContext, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { foodStack, treatStack } from "../utils/foodStack";
 import UserContext from "../utils/UserContext";
 import { getFoodFromPantry, getTreatFromPantry, getNextSupplement } from "../utils/feedingFunctions";
 import Meal from "./Meal";
 import useLocalStorage from '../utils/useLocalStorage';
+import Alert from '../common/Alert';
+import { vegetableOftenCheck, proteinOftenCheck, treatModeratelyCheck, supplementCheck } from '../utils/createTodos'
 import './Reptile.css';
 
 const Reptile = () => {
   const { reptiles, pantry } = useContext(UserContext)
   const { id, reptileId } = useParams();
   const navigate = useNavigate();
+  const [hasFood, setHasFood] = useState(false);
+  const [hasTreats, setHasTreats] = useState(false);
+
+  useEffect(() => {
+    function checkPantry() {
+      const hasProtein = proteinOftenCheck(pantry);
+      const hasVegetable = vegetableOftenCheck(pantry);
+      const hasSupplements = supplementCheck(pantry);
+      return hasProtein && hasVegetable && hasSupplements;
+    }
+    function checkTreats() {
+      return treatModeratelyCheck(pantry);
+    }
+    setHasFood(checkPantry());
+    setHasTreats(checkTreats());
+  }, [pantry]);
 
   const reptile = reptiles.filter(r => r.id === +reptileId)[0];
 
   const [meal, setMeal] = useState([]);
+  const [messages, setMessages] = useState([])
+
   const [displayFreq, setDisplayFreq] = useState(null);
   const [foodFreqs, setFoodFreqs] = useLocalStorage(`food-stack-for-${reptile.name}-${reptile.id}`);
   const [treatFreqs, setTreatFreqs] = useLocalStorage(`treat-stack-for-${reptile.name}-${reptile.id}`);
   const [lastSupplement, setLastSupplement] = useLocalStorage(`${reptile.name}'s-last-supplement-${reptile.id}`)
 
   const handleFeed = () => {
+    if (!hasFood) {
+      setMessages(["You don't have enough food in your pantry. See your account for more information"]);
+      setMeal([]);
+      return
+    }
     const stack = (!foodFreqs || !JSON.parse(foodFreqs).length) ? [...foodStack] : JSON.parse(foodFreqs);
     const freq = stack.pop();
     const protein = getFoodFromPantry(pantry, freq, "protein");
@@ -33,6 +58,11 @@ const Reptile = () => {
   };
   
   const handleTreat = () => {
+    if (!hasTreats) {
+      setMessages(["You don't have enough food in your pantry. See your account for more information"]);
+      setMeal([]);
+      return
+    }
     const stack = (!treatFreqs || !JSON.parse(treatFreqs).length) ? [...treatStack] : JSON.parse(treatFreqs);
     const freq = stack.pop();
     const treat = getTreatFromPantry(pantry, freq);
@@ -64,6 +94,7 @@ const Reptile = () => {
         <button className="treat" onClick={handleTreat}>Give a treat</button>
       </div>
 
+      {messages.length ? <Alert messages={messages} link={`/users/${+id}`} /> : null}
       {meal.length ? <Meal meal={meal} freq={displayFreq} /> : null}
     </div>
   );
