ba02e8d376c6c638456a4a8ab8ee7c219587844a
Adam Thometz <adamthometz@gmail.com>
2022-01-04 09:23:48 -0500

    added link capacity to alert component

--- frontend/src/common/Alert.js
+++ frontend/src/common/Alert.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './Alert.css'
 
-const Alert = ({ type = 'danger', messages = [] }) => {
+const Alert = ({ type = 'danger', messages = [], link = '' }) => {
+  const navigate = useNavigate();
   return (
-    <div className={`Alert ${type}`}>
+    <div className={`Alert ${type}`} onClick={link ? () => navigate(link) : null} style={ link ? {cursor: 'pointer'} : {}}>
       {messages.map((m, i) => (
         <p key={i}>{m}</p>
       ))}
