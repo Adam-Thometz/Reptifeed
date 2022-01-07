03034da9e0a06f2863e17b9bffc60571c1294126
Adam Thometz <adamthometz@gmail.com>
2022-01-07 07:58:25 -0500

    updated tests to account for todo functionality

--- frontend/src/user/Todos.test.js
+++ frontend/src/user/Todos.test.js
@@ -3,20 +3,12 @@ import { render, UserProvider } from '../utils/testUtils';
 import ReptifeedRoutes from "../routes-nav/ReptifeedRoutes";
 
 test('it renders without crashing', () => {
-  const todos = {
-    essentialTodos: ['Buy a plumbus'],
-    niceToHaveTodos: ['Figure out the plumbus']
-  };
   render(<UserProvider>
     <ReptifeedRoutes />
   </UserProvider>, { initialRoutes: ['/users/1/todos'] });
 });
 
 test('it matches snapshot', () => {
-  const todos = {
-    essentialTodos: ['Buy a plumbus'],
-    niceToHaveTodos: ['Figure out the plumbus']
-  };
   const { asFragment } = render(
     <UserProvider>
       <ReptifeedRoutes />
